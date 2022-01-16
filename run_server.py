5a3421320b178d02785eabf2efed1c76a94c1517
Kamilla Matisz <kmailla@pop-os.localdomain>
2022-01-16 13:26:52 +0100

    Add arg parser, add encoding vector as msg

--- run_server.py
+++ run_server.py
@@ -1,12 +1,13 @@
+import argparse
 from keras.models import load_model
+from model import get_encoding_vector
 import numpy as np
 import flask
-import sys
 
 # initialize Flask application
 app = flask.Flask(__name__)
 model = None
-MODEL_PATH = './saved_models/encoder.h5'
+MODEL_PATH = 'saved_models/classifier_model.h5'
 
 
 def run_model(model_path=MODEL_PATH):
@@ -17,6 +18,8 @@ def run_model(model_path=MODEL_PATH):
     """
     global model
     # load weights into new model
+    if model_path is None:
+        model_path = MODEL_PATH
     model = load_model(model_path)
     print("Model loaded.")
 
@@ -51,6 +54,9 @@ def predict():
             data['predicted_label'] = int(np.argmax(preds))
             data['model_score'] = float(max(preds[0]))
 
+            # add the encoding vector to the message
+            data['encoding_vector'] = get_encoding_vector(model, data_point).tolist()
+
             # indicate that the request was a success
             data['success'] = True
 
@@ -61,8 +67,12 @@ def predict():
 # load the model and hen start the server
 if __name__ == '__main__':
     print("* Loading Keras model and Flask starting server...")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_path', help='Path to the model')
+    args = parser.parse_args()
+
     # expecting the model path as a first arg
-    custom_model_path = sys.argv[1]
+    custom_model_path = args.model_path
     run_model(custom_model_path)
     app.config['JSON_AS_ASCII'] = False
     app.run(port=5002)
